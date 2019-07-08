cbb4a901114d12bb414a7730f0a57d3361df799d
lesterli <lester123@live.cn>
2019-07-08 22:17:34 +0800

    rust statement and variable

---
+++ src/variable.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let a = 1;
+    // 默认不可变绑定，不能重新赋值
+    // a = 2;
+    // 可使用mut关键字，创建可变绑定
+    println!("a {:p}", &a);
+    let mut b = 2;
+    b = 3;
+    println!("b was changed: {}", b);
+    
+    pub fn tmp() -> i32 {
+        return 1;
+    }
+    // 借用操作符&，获取表达式内存地址
+    let x = &tmp();
+    // 值表达式不能出现在位置上下文中，E0070
+    // tmp() = *x;
+    println!("x is memory address: {:p}", x);
+    // 声明动态数组，vec!
+    let mut c = vec![1,2,3];
+    // 使用借用操作符&，得到引用类型
+    let d = &mut c;
+    d.push(4);
+    println!("{:?}", d);
+    // 字面常量是值表达式，在位置上下文中求值创建临时值
+    let e = &42;
+    // 使用解引用操作符*，取得引用中的值
+    println!("reference e's value is {}", *e)
+}
