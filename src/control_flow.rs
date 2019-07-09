da38a8e970fddf4077995fd5c6beadca2651af27
lesterli <lester123@live.cn>
2019-07-09 20:21:03 +0800

    rust control flow

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -56,6 +56,31 @@ pub fn for_expr() {
     }
 }
 
+/**
+ * match表达式
+ */
+pub fn match_expr(n: i32) {
+    match n {
+        0 => println!("match number"),
+        1...3 => println!("match range"),
+        | 5 | 7 | 13  => println!("match branch"),
+        n @ 42 => println!("binding {}", n),
+        _ => println!("default"),
+    }
+}
+
+/**
+ * while let表达式
+ */
+pub fn while_let_pop() {
+    let mut v = vec![1,2,3];
+    // 动态数组的pop方法会返回Option类型，数组被取空会返回None
+    // 使用match表达式，需要匹配两种情况：Some(x)和None
+    while let Some(x) = v.pop() {
+        println!("{}", x);
+    }
+}
+
 fn main() {
     let x = 13;
     // Rust编译器根据上下文，会将结果截取
@@ -64,4 +89,15 @@ fn main() {
     while_expr();
     loop_expr();
     for_expr();
+
+    let mut n = 2;
+    match_expr(n);
+    n = 5;
+    match_expr(n);
+    n = 42;
+    match_expr(n);
+    n = 100;
+    match_expr(n);
+
+    while_let_pop();
 }
