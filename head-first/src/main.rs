bc923fd32d2fa910904a1ee103b8796dc8785222
lesterli <lester123@live.cn>
2020-01-09 12:09:24 +0800

    Send and Sync example

--- head-first/src/main.rs
+++ head-first/src/main.rs
@@ -56,6 +56,22 @@
 //    foo(&cell);
 //    cell.set(new_value); // oops, we clobbered the work done by foo
 
+use std::thread;
+use std::time::Duration;
 
+fn main() {
+    let t = thread::Builder::new()
+        .name("child1".to_string())
+        .spawn(move || {
+            println!("enter child thread.");
+            thread::park();
+            println!("resume child thread");
+        }).unwrap();
+    println!("spawn a thread");
+    thread::sleep(Duration::new(5,0));
+    t.thread().unpark();
+    t.join();
+    println!("child thread finished");
+}
 
 
