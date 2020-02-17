10e40c3736d0b4c4fb04334c83d85ad30e43f3c6
lesterli <lester123@live.cn>
2020-02-17 14:54:47 +0800

    add tarpc example

---
+++ head-first/tarpc/src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright 2018 Google LLC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// This is the service definition. It looks a lot like a trait definition.
+/// It defines one RPC, hello, which takes one arg, name, and returns a String.
+#[tarpc::service]
+pub trait World {
+    /// Returns a greeting for name.
+    async fn hello(name: String) -> String;
+}
