cbb1d58ac7197f54ad76c6c4432cf508c6aa5803
lesterli <lester123@live.cn>
2020-02-20 09:59:14 +0800

    async primer: multi-threading example codes

---
+++ head-first/async-primer/multi-example/src/file.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+pub fn read_file(path: &str) -> io::Result<String> {
+    let mut file = File::open(path)?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
