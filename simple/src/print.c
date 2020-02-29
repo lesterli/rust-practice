931d8b7d3e121b851b534b7be30e57468d6b2c51
lesterli <lester123@live.cn>
2020-02-29 23:03:39 +0800

    print pointer

--- simple/src/print.c
+++ simple/src/print.c
@@ -3,8 +3,10 @@
 #include <stdio.h> // for printf
 
 int main(int argc, char **argv) {
+    printf("argv = %p\n", argv); // new!
     for (int i = 0; i < argc; i++) {
         char *arg = argv[i];
+        printf("argv[%d] = %p\n", i, argv[i]); // new!
         printf("%s\n", arg);
     }
 
