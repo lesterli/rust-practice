c849a8751d40e08b33b894face59646dfe8280ff
lesterli <lester123@live.cn>
2020-03-01 00:53:34 +0800

    revert

--- simple/src/print.c
+++ simple/src/print.c
@@ -1,22 +1,13 @@
-// in `print.c`
-//lyys-MacBook-Pro:src lyy$ ./print "ready" "set" "go"
-//./printreadys
-//readysetgoTE
-//setgoTERM_PRO
-//goTERM_PROGRAM
-//$ # note: "-g 1" means "show groups of one byte",
-//$ # xxd defaults to "-g 2".
-// lyys-MacBook-Pro:src lyy$ ./print "ready" "set" "go" | xxd -g 1
-//00000000: 2e 2f 70 72 69 6e 74 00 72 65 61 64 79 00 73 0a  ./print.ready.s.
-//00000010: 72 65 61 64 79 00 73 65 74 00 67 6f 00 54 45 0a  ready.set.go.TE.
-//00000020: 73 65 74 00 67 6f 00 54 45 52 4d 5f 50 52 4f 0a  set.go.TERM_PRO.
-//00000030: 67 6f 00 54 45 52 4d 5f 50 52 4f 47 52 41 4d 0a  go.TERM_PROGRAM.
+//lyys-MacBook-Pro:src lyy$ ./print "élément"
+//� � l � � m e n t 
+//lyys-MacBook-Pro:src lyy$ ./print "élément"
+//élément
+//lyys-MacBook-Pro:src lyy$ ./print "élément" | xxd -g 1
+//00000000: c3 a9 6c c3 a9 6d 65 6e 74 0a                    ..l..ment.
 
 #include <stdio.h> // printf
-#include <ctype.h> // toupper
 
 int main(int argc, char **argv) {
-    // start from 1, skips program name
     for (int i = 1; i < argc; i++) {
         char *arg = argv[i];
         for (int j = 0;; j++) {
@@ -24,7 +15,8 @@ int main(int argc, char **argv) {
             if (character == 0) {
                 break;
             }
-            printf("%c", toupper(character));
+            // notice the space following `%c`
+            printf("%c", character);
         }
         printf("\n");
     }
