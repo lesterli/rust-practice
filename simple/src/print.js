20ce7c2e9deaeed9312addd296f48854a40325cb
lesterli <lester123@live.cn>
2020-03-01 00:57:08 +0800

    js upper

--- simple/src/print.js
+++ simple/src/print.js
@@ -5,9 +5,6 @@ const { argv, stdout } = process;
 // we have to skip *two* arguments: the path to node,
 // and the path to our script
 for (const arg of argv.slice(2)) {
-    for (const character of arg) {
-        stdout.write(character);
-        stdout.write(" ");
-    }
+    stdout.write(arg.toUpperCase());
     stdout.write("\n");
 }
