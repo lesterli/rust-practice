2e957be4d19a1c4c9380a6fe2b37b1d34605056a
lesterli <lester123@live.cn>
2020-03-01 21:26:07 +0800

    rust char

--- simple/src/string/rustre/src/main.rs
+++ simple/src/string/rustre/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let arg = std::env::args_os()
+    let arg = std::env::args()
         .skip(1)
         .next()
         .expect("should have one argument");
 
-    match arg.to_str() {
-        Some(arg) => println!("valid UTF-8: {}", arg),
-        None => println!("not valid UTF-8: {:?}", arg),
+    for c in arg.chars() {
+        print!("{} ", c);
     }
+    println!()
 }
