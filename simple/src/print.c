f79c4854234535b60d0acaf54dfd5e52c30abb64
lesterli <lester123@live.cn>
2020-03-01 11:47:38 +0800

    print utf-8

--- simple/src/print.c
+++ simple/src/print.c
@@ -1,23 +1,45 @@
-//lyys-MacBook-Pro:src lyy$ ./print "élément"
-//� � l � � m e n t 
-//lyys-MacBook-Pro:src lyy$ ./print "élément"
-//élément
-//lyys-MacBook-Pro:src lyy$ ./print "élément" | xxd -g 1
-//00000000: c3 a9 6c c3 a9 6d 65 6e 74 0a                    ..l..ment.
+// in `print.c`
 
 #include <stdio.h> // printf
+#include <stdint.h> // uint8_t
+
+void print_spaced(char *s) {
+    // start at the beginning
+    int i = 0;
+
+    while (1) {
+        // we're going to be shifting bytes around,
+        // so treat them like unsigned 8-bit values
+        uint8_t c = s[i];
+        if (c == 0) {
+            // reached null terminator, stop printing
+            break;
+        }
+
+        // length of the sequence, ie., number of bytes
+        // that encode a single Unicode scalar value
+        int len = 1;
+        if (c >> 5 == 0b110) {
+            len = 2;
+        } else if (c >> 4 == 0b1110) {
+            len = 3;
+        } else if (c >> 3 == 0b11110) {
+            len = 4;
+        }
+
+        // print the entire UTF-8-encoded Unicode scalar value
+        for (; len > 0; len--) {
+            printf("%c", s[i]);
+            i++;
+        }
+        // print space separator
+        printf(" ");
+    }
+}
 
 int main(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
-        char *arg = argv[i];
-        for (int j = 0;; j++) {
-            char character = arg[j];
-            if (character == 0) {
-                break;
-            }
-            // notice the space following `%c`
-            printf("%c", character);
-        }
+        print_spaced(argv[i]);
         printf("\n");
     }
 
