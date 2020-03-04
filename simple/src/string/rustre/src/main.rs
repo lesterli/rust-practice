cb02d99057e212f1b46eb93dcb7488826b9ee8e0
lesterli <lester123@live.cn>
2020-03-04 21:49:33 +0800

    Rust string upper

--- simple/src/string/rustre/src/main.rs
+++ simple/src/string/rustre/src/main.rs
@@ -4,10 +4,19 @@ fn main() {
         .next()
         .expect("should have one argument");
 
-    println!("upp = {}", uppercase(&arg));
+    let mut upp = String::new();
+    // was just `upp`
+    uppercase(&arg, &mut upp);
+
+    println!("upp = {}", upp);
     println!("arg = {}", arg);
 }
 
-fn uppercase(s: &str) -> String {
-    s.to_uppercase()
+// was `mut dst: String`
+fn uppercase(src: &str, dst: &mut String) {
+    for c in src.chars() {
+        for c in c.to_uppercase() {
+            dst.push(c);
+        }
+    }
 }
