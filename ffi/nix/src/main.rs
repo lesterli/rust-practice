c0258a4a6919f96d8572803a4d1891cfb3ac2221
lesterli <lester123@live.cn>
2020-04-29 23:52:32 +0800

    nix fork/kill example

--- ffi/nix/src/main.rs
+++ ffi/nix/src/main.rs
@@ -1,21 +1,37 @@
 extern crate nix;
 
+// use nix::unistd::*;
+
+// fn main() {
+//     match fork() {
+//         Ok(ForkResult::Parent { child }) => {
+//             // 在父进程中
+//             println!("Hello, I am parent thread: {}", getpid());
+//         }
+//         Ok(ForkResult::Child) => {
+//             // 在子进程中
+//             println!("Hello, I am child thread: {}", getpid());
+//             println!("My parent thread: {}", getppid());            
+//         }
+//         Err(errno) => {
+//             // fork 创建子进程失败
+//             println!("Fork creation failed!");
+//         }
+//     }  
+// }
+
+use nix::sys::signal::*;
 use nix::unistd::*;
 
 fn main() {
-    match fork() {
-        Ok(ForkResult::Parent { child }) => {
-            // 在父进程中
-            println!("Hello, I am parent thread: {}", getpid());
+    match fork().expect("fork failed") {
+        ForkResult::Parent{ child } => {
+            sleep(5);
+            kill(child, SIGKILL).expect("kill failed");
         }
-        Ok(ForkResult::Child) => {
-            // 在子进程中
-            println!("Hello, I am child thread: {}", getpid());
-            println!("My parent thread: {}", getppid());            
+        ForkResult::Child => {
+            // 直到被 kill 掉
+            loop {}
         }
-        Err(errno) => {
-            // fork 创建子进程失败
-            println!("Fork creation failed!");
-        }
-    }  
+    }
 }
