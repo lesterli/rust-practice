a0fe255c6c36693ab325559f8dd3fc86da048690
lesterli <lester123@live.cn>
2020-06-18 15:28:15 +0800

    change u8 to c_char

--- ffi/example_09/src/main.rs
+++ ffi/example_09/src/main.rs
@@ -1,40 +1,12 @@
-use std::ffi::CString;
-use std::os::raw::{c_float, c_int};
+use std::os::raw::{c_char, c_float, c_int};
 
 #[repr(C)]
 #[derive(Debug)]
 pub struct CStudent {
     pub num: c_int,
     pub total: c_int,
-    pub name: [u8; 20],
+    pub name: [c_char; 20],
     pub scores: [c_float; 3],
-}
-
-impl CStudent {
-    // constructor with parameters
-    pub fn new(num: i32, total: i32, name: String, scores: Vec<f32>) -> CStudent {
-        let c_string = CString::new(name).expect("CString::new failed");
-        let bytes = c_string.as_bytes_with_nul();
-        
-        let mut c_name: [u8; 20] = [0u8; 20];
-        c_name[..bytes.len()].copy_from_slice(bytes);
-
-        let mut c_scores: [f32; 3] = [0f32; 3];
-        for (index, score) in scores.iter().enumerate() {
-            if index > 2 {
-                break;
-            } else {
-                c_scores[index] = *score as c_float;
-            }
-        }
-
-        CStudent {
-            num,
-            total,
-            name: c_name,
-            scores: c_scores,
-        }
-    }
 }
 
 // Default constructor
@@ -43,7 +15,7 @@ impl Default for CStudent {
         CStudent {
             num: 0 as c_int,
             total: 0 as c_int,
-            name: [0u8; 20],
+            name: [0 as c_char; 20],
             scores: [0.0 as c_float; 3],            
         }
     }
@@ -66,7 +38,7 @@ fn main() {
     unsafe {
         fill_data(p_stu);
         print_data(p_stu);
+        //Box::from_raw(p_stu);
         println!("rust side print Bob: {:?}", Box::from_raw(p_stu));
-        
     }
 }
