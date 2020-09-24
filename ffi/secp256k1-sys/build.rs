729550ff5895232fb36f47f6afb473c821c08e13
lesterli <lester123@live.cn>
2020-09-24 15:29:30 +0800

    change crate name

--- ffi/secp256k1-sys/build.rs
+++ ffi/secp256k1-sys/build.rs
@@ -11,7 +11,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
